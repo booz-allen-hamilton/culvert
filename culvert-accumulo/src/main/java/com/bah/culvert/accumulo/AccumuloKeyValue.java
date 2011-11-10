c119971b42fcc785d108e44a010b95d2d47644ec
Jesse Yates <jesse.k.yates@gmail.com>
2011-11-10 09:47:27 -0800

    Adding licenses to accumulo files

--- culvert-accumulo/src/main/java/com/bah/culvert/accumulo/AccumuloKeyValue.java
+++ culvert-accumulo/src/main/java/com/bah/culvert/accumulo/AccumuloKeyValue.java
@@ -1,9 +1,29 @@
+/**
+ * Copyright 2011 Booz Allen Hamilton.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  Booz Allen Hamilton licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.bah.culvert.accumulo;
 
+import com.bah.culvert.accumulo.database.AccumuloTableAdapter;
 import com.bah.culvert.data.CKeyValue;
 
 /**
- * A Culvert {@link CKeyValue} that also maintains the visibility of the Key/Value. This key can be used interchangeably with respect to a regular {@link CKeyValue} for other TableAdapters, but visibility is taken into account when using a connection via a {@link CloudbaseTableAdapter} 
+ * A Culvert {@link CKeyValue} that also maintains the visibility of the
+ * Key/Value. This key can be used interchangeably with respect to a regular
+ * {@link CKeyValue} for other TableAdapters, but visibility is taken into
+ * account when using a connection via a {@link AccumuloTableAdapter}
  */
 public class AccumuloKeyValue extends CKeyValue {
 
@@ -16,7 +36,7 @@ public class AccumuloKeyValue extends CKeyValue {
    * @param row
    * @param family
    * @param qualifier
-   * @param bs
+   * @param vis
    * @param timestamp
    * @param value
    */
@@ -24,7 +44,7 @@ public class AccumuloKeyValue extends CKeyValue {
       String vis, long timestamp, byte[] value) {
     super(row, family, qualifier, timestamp, value);
     this.visibility = vis;
-    }
+  }
 
   /**
    * Create the {@link CKeyValue} for accumulo that just uses the empty, "",
@@ -36,11 +56,10 @@ public class AccumuloKeyValue extends CKeyValue {
    * @param value
    */
   public AccumuloKeyValue(byte[] row, byte[] family, byte[] qualifier,
-      long timestamp,
-      byte[] value) {
+      long timestamp, byte[] value) {
     this(row, family, qualifier, DEFAULT_VISIBILTY, timestamp, value);
   }
-  
+
   public AccumuloKeyValue(byte[] rowId) {
     super(rowId);
     this.visibility = DEFAULT_VISIBILTY;
@@ -50,17 +69,19 @@ public class AccumuloKeyValue extends CKeyValue {
     this(rowId, family, qualifier, DEFAULT_VISIBILTY);
   }
 
-  public AccumuloKeyValue(byte[] rowId, byte[] family, byte[] qualifier, String vis) {
+  public AccumuloKeyValue(byte[] rowId, byte[] family, byte[] qualifier,
+      String vis) {
     super(rowId, family, qualifier);
     this.visibility = vis;
   }
 
-  public AccumuloKeyValue(byte[] rowId, byte[] family, byte[] qualifier, byte[] value) {
+  public AccumuloKeyValue(byte[] rowId, byte[] family, byte[] qualifier,
+      byte[] value) {
     this(rowId, family, qualifier, DEFAULT_VISIBILTY, value);
   }
 
-  public AccumuloKeyValue(byte[] rowId, byte[] family, byte[] qualifier, String vis,
-      byte[] value) {
+  public AccumuloKeyValue(byte[] rowId, byte[] family, byte[] qualifier,
+      String vis, byte[] value) {
     super(rowId, family, qualifier, value);
     this.visibility = vis;
   }
--- culvert-accumulo/src/main/java/com/bah/culvert/accumulo/database/AccumuloDatabaseAdapter.java
+++ culvert-accumulo/src/main/java/com/bah/culvert/accumulo/database/AccumuloDatabaseAdapter.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2011 Booz Allen Hamilton.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  Booz Allen Hamilton licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.bah.culvert.accumulo.database;
 
 import static com.bah.culvert.accumulo.AccumuloConstants.DEFAULT_MAX_LATENCY;
@@ -80,8 +96,6 @@ public class AccumuloDatabaseAdapter extends DatabaseAdapter {
 
   @Override
   public TableAdapter getTableAdapter(String tableName) {
-    // TODO have to double check here to make sure that reusing the connection
-    // isn't going to be a problem
     Configuration conf = this.getConf();
     return new AccumuloTableAdapter(this.conn, tableName, conf.getLong(
         MAX_MEMORY_KEY, DEFAULT_MAX_MEMORY), conf.getLong(MAX_LATENCY_KEY,
